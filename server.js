ac9b19817638bce82889af54383432ee5bbb7696
kberwyn <k.berwyn@gmail.com>
2025-08-08 17:33:39 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Rugby Availability API is running!' });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
